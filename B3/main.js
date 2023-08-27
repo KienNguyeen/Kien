7729264d2addbf2d2124ac051c9bf93d9d2ef5f0
KienNguyeen <kienkinhcan2k7@gmail.com>
2023-08-27 12:12:59 +0700

    Message(B4)

---
+++ B3/main.js
@@ -0,0 +1,14 @@
+let arr = ["toán","ngữ văn", "tiếng anh"]
+
+for (let i in arr) {
+    console.log (arr[i])
+}
+let obj = {
+    'name': "Nguyễn Văn Đấu",
+    'sex': "không xác định",
+    'crime': "vi phạm hành chính"
+}
+for (let i in obj)
+console.log(obj[i])
+for (let value of arr)
+console.log(value)
